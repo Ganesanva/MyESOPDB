3590522e1dcb6393de3eb645b0199cbc11490960
vairavan <vairavan@geopits.com>
2022-07-13 14:16:39 +0530

    Latest committed

--- KPED_Geo/CHANGE/User/User.sql
+++ KPED_Geo/CHANGE/User/User.sql
@@ -4,4 +4,28 @@ DROP USER IF EXISTS [MYESOPSPRDDB\karuna];
 
 DROP USER IF EXISTS  [Vairavan];
 
+/*
+IF not exists (
+SELECT 'X'
+FROM sys.database_principals
+WHERE name = '[MyESOPsENCDB\amit]'
+)
+begin 
+CREATE USER [MyESOPsENCDB\amit] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[dbo]
+end 
+go
+
+
+IF not exists (
+SELECT 'X'
+FROM sys.database_principals
+WHERE name = '[MyESOPsEncDB\SuperUser]'
+)
+begin 
+CREATE USER [MyESOPsEncDB\SuperUser] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[dbo]
+end 
+go
+*/
+
+
 Set NOCOUNT OFF
